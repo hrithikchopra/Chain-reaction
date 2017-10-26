c4cb1d008c48ea27dff44aec1610e0129078d7d5
hrithik chopra <hrithik16237@iiitd.ac.in>
2017-10-26 19:20:39 +0530

    Added an exception which will be thrown when the user sets the same color for two players

---
+++ src/application/SameColorException.java
@@ -0,0 +1,9 @@
+package application;
+
+public class SameColorException extends Exception{
+	private static final long serialVersionUID = 1L;
+	SameColorException(String message){
+		super(message);
+	}
+}
+
